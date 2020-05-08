22951802e3fe11d3191fcd329aa190ec33d136ac
Chris Lessard <chris.lessard.96@gmail.com>
2020-05-08 11:38:45 -0400

    Reconfigure the database as a class

--- database.py
+++ database.py
@@ -1,31 +1,40 @@
 FILENAME = 'database.bin'
 
-def db_set(key, value):
-    '''
-    Stores a new key value pair in the DB
-    '''
+class Database():
+    database_name = None
 
-    pass
+    def __init__(self, database_name):
+        self.database_name = database_name
 
-def db_get(key):
-    '''
-    Retrieve the value associated with key in the db
-    '''
-    pass
+    def db_set(self, key, value):
+        '''
+        Stores a new key value pair in the DB
+        '''
+
+        pass
+
+    def db_get(self, key):
+        '''
+        Retrieve the value associated with key in the db
+        '''
+        pass
 
 def main():
     '''
     Run the database interface.
     '''
     usage_msg = 'Please select an option: \n\tstore {key} {data}\n\tget {key}\n\texit'
+
+    db = Database(FILENAME)
+
     while True:
         print(usage_msg)
         cmd = input('$ ').lower().split(' ')
 
         if cmd[0] == 'store':
-            db_set(cmd[1], cmd[2])
+            db.db_set(cmd[1], cmd[2])
         elif cmd[0] == 'get':
-            db_get(cmd[1])
+            db.db_get(cmd[1])
         elif cmd[0] == 'exit':
             break
         else:
