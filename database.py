8bb2f2f864c4b3e837d0fc95ec7ceb6c470a1766
Chris Lessard <chris.lessard.96@gmail.com>
2020-05-08 16:42:24 -0400

    Add functionality for loading index from database, as well as storing/loading a current snapshot of the index on disk

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pickle
 
 FILENAME = 'database.bin'
 
@@ -11,7 +12,7 @@ class Database():
         self.index = {}
 
     def db_set(self, key, value):
-        '''
+        ''' (self, int, string) => None
         Stores a new key value pair in the DB
         '''
         log = str(key) + ',' + (value) + '\n'
@@ -21,7 +22,7 @@ class Database():
             s.write(log)
 
     def db_get(self, key):
-        '''
+        ''' (self, int) => None
         Retrieve the value associated with key in the db
         '''
         offset = self.is_indexed(key)
@@ -37,26 +38,66 @@ class Database():
                     if int(k) == key:
                         return v.strip()
 
-    
     def is_indexed(self, key):
+        ''' (self, int) => Bool
+        Checks whether key is stored in the DB's index
+        '''
         return self.index[key] if key in self.index.keys() else None
+
+    def load_index(self):
+        ''' (self) => None
+        Parses the database file and loads keys into the index. Warning, this is really slow!
+        '''
+        with open(self.db_name, 'r') as s:
+            for line in s:
+                k, v = line.split(',')
+                self.index[k] = v
+
+    def save_index_snapshot(self, name):
+        ''' (self, str) => None
+        Saves a pickle dump of the current index to disk, calling the file name.
+        '''
+        with open(name, 'wb') as snapshot_file_stream:
+            pickle.dump(self.index, snapshot_file_stream)
+
+    def load_index_snapshot(self, name):
+        ''' (self, str) => None
+        Loads a snapshot of the index from disk to memory, using file name.
+        '''
+        with open(name, 'rb') as snapshot_file_stream:
+            self.index = pickle.load(snapshot_file_stream)
 
 def main():
     '''
     Run the database interface.
     '''
-    usage_msg = 'Please select an option: \n\tstore {key} {data}\n\tget {key}\n\texit'
+    usage_msg = [
+        'Please select an option: ',
+        'store {key} {data}',
+        'get {key}',
+        'save_index {filename}',
+        'load_index {filename}',
+        'exit'
+    ]
 
     db = Database(FILENAME)
 
     while True:
-        print(usage_msg)
+        print('\n\t'.join(usage_msg))
         cmd = input('$ ').lower().split(' ')
 
         if cmd[0] == 'store':
-            db.db_set(cmd[1], cmd[2])
+            key, val = cmd[1], cmd[2]
+            db.db_set(key, val)
         elif cmd[0] == 'get':
-            db.db_get(cmd[1])
+            key = cmd[1]
+            db.db_get(key)
+        elif cmd[0] == 'save_index':
+            name = cmd[1]
+            db.save_index_snapshot(name)
+        elif cmd[0] == 'load_index':
+            name = cmd[1]
+            db.load_index_snapshot(name)
         elif cmd[0] == 'exit':
             break
         else:
